29767dc91b572f6e7c1f14e67af55c11517052f9
drew verlee <drew.verlee@gmail.com>
2018-04-21 20:28:32 -0400

    code failing when run with lein test

--- test/fk_gen/core_test.clj
+++ test/fk_gen/core_test.clj
@@ -1,7 +1,46 @@
 (ns fk-gen.core-test
   (:require [clojure.test :refer :all]
-            [fk-gen.core :refer :all]))
+            [fk-gen.core :as fk-gen]
+            [honeysql.core :as sql]
+            [clojure.java.jdbc :as jdbc])
+  (:import [com.opentable.db.postgres.embedded EmbeddedPostgres]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn with-postgres [f]
+  (let [db (-> (EmbeddedPostgres/builder)
+               (.setPort 3001)
+               .start)
+        ]
+    (try
+      (f)
+      (finally
+        (.close db)))))
+
+(use-fixtures :each with-postgres)
+
+(deftest test-create
+  (testing "creation of dependency graph sql data."
+    (let [db-info {:classname "org.postgresql.Driver"
+                   :subprotocol "postgresql"
+                   :subname "//localhost:3001/postgres"
+                   :schema "public"
+                   :user "postgres"
+                   :sslfactory "org.postgresql.ssl.NonValidatingFactory"}
+
+          create-persons-table "CREATE TABLE persons (id serial primary key,
+                                                      name text);"
+
+          create-dogs-table "CREATE TABLE dogs (id serial primary key,
+                                                name text,
+                                                owner integer references persons(id));"
+          ]
+      (do (jdbc/execute! db-info create-persons-table)
+          (jdbc/execute! db-info create-dogs-table)
+
+          ;; create foreign key deps for the dogs table and insert them into the db.
+          (->> (fk-gen/create :dogs db-info) 
+               flatten
+               (map #(sql/format %))
+               (map #(jdbc/execute! db-info %)))
+          (Thread/sleep 1000)
+          )
+      (is (= 1 (jdbc/query db-info "select COUNT(*) from dogs;"))))))
