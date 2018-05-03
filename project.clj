7c4527dbbe1354b7c5018dda0de05f047dd97987
drew verlee <drew.verlee@gmail.com>
2018-05-02 21:30:03 -0400

    fairly large refactor to allow for more flexability

--- project.clj
+++ project.clj
@@ -6,8 +6,9 @@
                  [com.layerware/hugsql "0.4.8"]
                  [org.postgresql/postgresql "42.2.2"]
                  [com.rpl/specter "1.1.1"]
-                 [org.clojure/java.jdbc "0.7.5"]
+                 [org.clojure/java.jdbc "0.7.6"]
                  [viesti/table-spec "0.1.1"]
                  [nilenso/honeysql-postgres "0.2.3"]
+                 [datascript "0.16.4"]
                  [org.clojure/test.check "0.9.0"]]
-  :profiles {:dev {:dependencies  [[com.opentable.components/otj-pg-embedded "0.11.4"]]}}) 
+  :profiles {:dev {:dependencies  [[com.opentable.components/otj-pg-embedded "0.12.0"]]}}) 
--- src/fk_gen/core.clj
+++ src/fk_gen/core.clj
@@ -9,13 +9,13 @@
             [table-spec.core :as t]
             [clojure.spec.alpha :as s]
             [clojure.spec.gen.alpha :as gen]
+            [datascript.core :as d]
             [com.rpl.specter :refer [transform MAP-VALS ALL]]
             [clojure.string :as str]))
 
-;; defines get-fk-deps
+;; defines function get-fk-deps
 (hugsql/def-db-fns "fk-deps.sql")
 
-;;TODO make sure handling the case of no deps probably merge or something or change sql? graph should have everything
 (defn- fk-deps->graph
   [fk-deps]
   (reduce-kv (fn [c fk v]
@@ -34,26 +34,17 @@
      (when n (cons (f n g) (dfs->path (filterv #(not (v %)) (concat (pop nxs) (keys (g n)))) f v g))))))
 
 (defn- fk-deps->sql-plan
-  [fk-deps table table-graph->insert-stmt-plan]
+  [{:keys [table table-graph->insert-stmt-plan fk-deps]}]
   (->> fk-deps
-      tables->graph
-      (dfs->path table table-graph->insert-stmt-plan)
-      reverse))
+       fk-deps->graph
+       (dfs->path table table-graph->insert-stmt-plan)
+       reverse))
 
-
-(s/def ::table keyword?)
-;;(s/def ::db-info )
-
-;;TODO add spec
-;;TODO should i pass db-info to everything or so they can query it?
 (defn create
- "Returns a vector of insert statements necessary to fulfill all the foreign key constraints of the given table
-  table   :keyword : the root of the dependency tree graph you want to generate
-  db-info :hashmap : a map describing the database connection information"
-  [table db-info]
-  (->> db-info
-       get-fk-deps
-       fk-deps->sql-plan))
-
-
-
+  "Returns a vector of sql insert statement (honeysql format) necessary to fulfill all the foreign key constraints of the given table
+  `table`                         :keyword : the name of the table
+  `db-info`                       :hashmap : a map describing the database connection information see https://github.com/clojure/java.jdbc.
+  `table-graph->insert-stmt-plan` :fn      : a two arity function that takes a table and graph and returns a vector of honeysql formatted insert statements
+  "
+  [{:keys [db-info table table-graph->insert-stmt-plan]}]
+  (fk-deps->sql-plan {:table table :table-graph->insert-stmt-plan table-graph->insert-stmt-plan :fk-deps (get-fk-deps db-info)}))
